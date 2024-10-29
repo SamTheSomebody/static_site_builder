ed9b08a15c12bd0f318d0f97f8dd7277d20127a7
SamTheSomebody <gamedevsamdevelopment+github@proton.me>
2024-10-29 16:12:42 +1100

    Updated process markdown requirements and tests

--- src/processmarkdown.py
+++ src/processmarkdown.py
@@ -6,11 +6,13 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
         if node.text_type != TextType.NORMAL.value:
             nodes.append(node)
             continue
-        if delimiter not in node.text:
-            raise ValueError(f"'{delimiter}' not found in '{node.text}'")
         split_text = node.text.split(delimiter)
+        if len(split_text) % 2 == 0:
+            raise ValueError("Invalid markdown, formatted section not closed")
         for i in range(0, len(split_text)-1, 2):
-            nodes.append(TextNode(split_text[i], TextType.NORMAL))
-            nodes.append(TextNode(split_text[i+1], text_type))
+            if split_text[i] != "":
+                nodes.append(TextNode(split_text[i], TextType.NORMAL))
+            if split_text[i+1] != "":
+                nodes.append(TextNode(split_text[i+1], text_type))
         nodes.append(TextNode(split_text[-1], TextType.NORMAL))
     return nodes
--- src/test_processmarkdown.py
+++ src/test_processmarkdown.py
@@ -5,40 +5,67 @@ from textnode import TextNode, TextType
 
 class TestProcessMarkdown(unittest.TestCase):
     def test_bold_text(self):
-        markdown = "This is a *test* string."
-        node = TextNode(markdown, TextType.NORMAL)
-        nodes = [node]
-        result = split_nodes_delimiter(nodes, "*", TextType.BOLD)
-        self.assertEqual(len(result), 3)
-
-    def test_italic_text(self):
         markdown = "This is a **test** string."
         node = TextNode(markdown, TextType.NORMAL)
         nodes = [node]
-        result = split_nodes_delimiter(nodes, "**", TextType.ITALIC)
-        self.assertEqual(len(result), 3)
+        result = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+        expected = [
+            TextNode("This is a ", TextType.NORMAL),
+            TextNode("test", TextType.BOLD),
+            TextNode(" string.", TextType.NORMAL),
+        ]
+        self.assertListEqual(result, expected)
+
+    def test_italic_text(self):
+        markdown = "This is a *test* string."
+        node = TextNode(markdown, TextType.NORMAL)
+        nodes = [node]
+        result = split_nodes_delimiter(nodes, "*", TextType.ITALIC)
+        expected = [
+            TextNode("This is a ", TextType.NORMAL),
+            TextNode("test", TextType.ITALIC),
+            TextNode(" string.", TextType.NORMAL),
+        ]
+        self.assertListEqual(result, expected)
 
     def test_code_text(self):
         markdown = "This is a `test` string."
         node = TextNode(markdown, TextType.NORMAL)
         nodes = [node]
         result = split_nodes_delimiter(nodes, "`", TextType.CODE)
-        self.assertEqual(len(result), 3)
+        expected = [
+            TextNode("This is a ", TextType.NORMAL),
+            TextNode("test", TextType.CODE),
+            TextNode(" string.", TextType.NORMAL),
+        ]
+        self.assertListEqual(result, expected)
 
     def test_multiple_bold(self):
-        markdown = "This *is a *test* string*."
+        markdown = "This **is a **test** string**."
         node = TextNode(markdown, TextType.NORMAL)
         nodes = [node]
-        result = split_nodes_delimiter(nodes, "*", TextType.BOLD)
-        self.assertEqual(len(result), 5)
+        result = split_nodes_delimiter(nodes, "**", TextType.BOLD)
+        expected = [
+            TextNode("This ", TextType.NORMAL),
+            TextNode("is a ", TextType.BOLD),
+            TextNode("test", TextType.NORMAL),
+            TextNode(" string", TextType.BOLD),
+            TextNode(".", TextType.NORMAL),
+        ]
+        self.assertListEqual(result, expected)
 
-#    def test_nested_text(self):
-#        markdown = "This **is a *test* string.**"
-#        node = TextNode(markdown, TextType.NORMAL)
-#        nodes = [node]
-#        result = split_nodes_delimiter(nodes, "*", TextType.BOLD)
-#        result = split_nodes_delimiter(result, "**", TextType.ITALIC)
-#        self.assertEqual(len(result), 5)
+    def test_nested_text(self):
+        markdown = "This `is a **test** string.`"
+        node = TextNode(markdown, TextType.NORMAL)
+        nodes = [node]
+        result = split_nodes_delimiter(nodes, "`", TextType.CODE)
+        result = split_nodes_delimiter(result, "**", TextType.BOLD)
+        expected = [
+            TextNode("This ", TextType.NORMAL),
+            TextNode("is a **test** string.", TextType.CODE),
+            TextNode("", TextType.NORMAL),
+        ]
+        self.assertListEqual(result, expected)
 
 if __name__ == "__main__":
     unittest.main()
