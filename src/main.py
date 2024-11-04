66209c2ee9561914565085a7e205327cc1876568
SamTheSomebody <gamedevsamdevelopment+github@proton.me>
2024-11-04 20:08:41 +1100

    Added file copy functionality for generating webpages

--- src/main.py
+++ src/main.py
@@ -1,6 +1,34 @@
-from text_node import TextNode, TextType
+import os
+import shutil
+
 def main():
-    text_node = TextNode("test", TextType.BOLD, "boot.dev")
-    print(text_node)
+    dest_path = os.path.realpath("public")
+    print(f"Destination path is: {dest_path}")
+    src_path = os.path.realpath("static")
+    print(f"Source path is: {src_path}")
+
+    if os.path.exists(dest_path):
+        print(f"Removing directory: {dest_path}")
+        shutil.rmtree(dest_path, ignore_errors = False)
+    os.mkdir(dest_path)
+
+    copy_file(src_path)
+    #Recursive Function
+    #Move all files and folders from static to public
+    #Delete all files in public
+
+def copy_file(path):
+    print(f"Looking through: {path}")
+    sub_paths = os.listdir(path)
+    for sub_path in sub_paths:
+        print(sub_path)
+        full_path = os.path.join(path, sub_path)
+        dest_path = full_path.replace("/static/", "/public/")
+        print(f"Copying: {full_path} to: {dest_path}")
+        if os.path.isfile(full_path):
+            shutil.copy(full_path, dest_path)
+        else:
+            os.mkdir(dest_path)
+            copy_file(full_path)
 
 main()
